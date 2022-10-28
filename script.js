03c2aa759c2c9d07ed185d28de5ce223cd309b88
AlecLimaDev <aleclimadev@gmail.com>
2022-10-28 16:49:08 -0300

    animacao de mouse criada

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const cursor1 = document.querySelector('.cursor-1');
+const cursor2 = document.querySelector('.cursor-2');
+
+onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
