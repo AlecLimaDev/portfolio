68cc4334729c527db62388130cdf63c93a5a1f06
AlecLimaDev <aleclimadev@gmail.com>
2022-10-30 15:50:13 -0300

    codigo apagado

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 
-const cursor1 = document.querySelector('.cursor-1');
-const cursor2 = document.querySelector('.cursor-2');
-
-onmousemove = (e) => {
-    cursor1.style.top = e.pageY + 'px';
-    cursor1.style.left = e.pageX + 'px';
-    cursor2.style.top = e.pageY + 'px';
-    cursor2.style.left = e.pageX + 'px';
-}
-
