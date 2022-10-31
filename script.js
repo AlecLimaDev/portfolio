5eb5251359a9e46da0c93f186abf7b7257fc0216
AlecLimaDev <aleclimadev@gmail.com>
2022-10-31 13:20:23 -0300

    adicionado interação de onclick no menu ao clicar

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let menu = document.querySelector('.#menu-btn');
+let header = document.querySelector('.header');
 
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('actives');
+}
