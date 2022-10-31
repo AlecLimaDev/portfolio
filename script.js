5c36c1d840a8316c0262e2e4e746184619f080b0
AlecLimaDev <aleclimadev@gmail.com>
2022-10-31 19:52:23 -0300

    feita interação que ao clicar no moon muda para o tema dark

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
 }
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () => {
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')) {
+        document.body.classList.add('active');
+    } else {
+        document.body.classList.remove('active');
+    }
+}
