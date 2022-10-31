33e99d6d2e19beda9134fc488b5334d2e02b9c91
AlecLimaDev <aleclimadev@gmail.com>
2022-10-31 15:44:53 -0300

    adicionado interação de abertura e fechamento de menu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let menu = document.querySelector('.#menu-btn');
+let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
-    header.classList.toggle('actives');
+    header.classList.toggle('active');
 }
